0a65c521c4497762e9ef55e24d060e2bfcd6e5fd
Brendo <brendow_prime@hotmail.com>
2024-04-16 01:59:34 -0400

    Feat: InfoContext created

--- src/App.jsx
+++ src/App.jsx
@@ -2,19 +2,20 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './App-Styles.jsx'
 import { Container } from './App-Styles.jsx'
 import HomePage from './pages/HomePage.jsx'
+import { InfoContext } from './contexts/InfoContext.jsx'
 
 function App() {
 
   return (
-    <>
-      <Container>
-        <BrowserRouter>
+    <Container>
+      <BrowserRouter>
+        <InfoContext.Provider value={('value')}>
           <Routes>
             <Route path="/" element={<HomePage />}></Route>
           </Routes>
-        </BrowserRouter>
-      </Container>
-    </>
+        </InfoContext.Provider>
+      </BrowserRouter>
+    </Container>
   )
 }
 
