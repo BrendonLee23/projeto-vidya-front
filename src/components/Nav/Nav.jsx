d3256cc8af0d4d8d8262e391e1a0425956362c92
Brendo <brendow_prime@hotmail.com>
2024-04-16 15:32:26 -0400

    Feat: Nav static version

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,12 +1,25 @@
 import { styled } from "styled-components"
+import SearchIcon from "../../assets/images/search.svg"
+import AddIcon from "../../assets/images/iconAdd.svg"
 
 export default function Nav(){
+
+
+
     return(
         <StyledNav>
-            Nav
+            <StyledInput>
+            <input type="text" placeholder="Pesquisar" />
+            <img src={SearchIcon} alt="SearchIcon" />
+            </StyledInput>
+            <StyledButton>
+                <img src={AddIcon} alt="AddIcon" />
+                <p>Novo Cliente</p>
+            </StyledButton>
         </StyledNav>
     )
 }
+
 
 const StyledNav = styled.div`
     display: flex;
@@ -14,7 +27,60 @@ const StyledNav = styled.div`
     justify-content: space-between;
     height: 70px;
     width: auto;
-    background-color: #014078;
+    background-color: #FFFFFF;
+    padding: 0px 20px 0px 20px;
     margin: 20px 30px 20px 30px;
     border-radius: 8px;
+`
+
+const StyledInput = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: end;
+    input{
+        width: 300px;
+        height: 40px;
+        padding: 19px 16px;
+        background-color: #FFFFFF;
+        border: 1px solid #5D6570;
+        border-radius: 8px;
+        position: relative;
+
+    }
+    input::placeholder {
+        font-family: Roboto;
+        font-size: 14px;
+        font-weight: 400;
+        line-height: 19px;
+        letter-spacing: 0em;
+        text-align: left;
+        padding-left: 13px;
+    }
+    img{
+        position: absolute;
+        margin-right: 20px;
+        cursor: pointer;
+    }
+`
+const StyledButton = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    background-color: #006FFD;
+    width: 160px;
+    height: 40px;
+    padding: 10px 24px 10px 16px;
+    gap: 11px;
+    border-radius: 5px;
+    opacity: 0px;
+    cursor: pointer;
+p{
+    font-family: Roboto;
+    color: #ffffff;
+    font-size: 14px;
+    font-weight: 500;
+    line-height: 20px;
+    letter-spacing: 0.10000000149011612px;
+    text-align: center;
+    }
 `
