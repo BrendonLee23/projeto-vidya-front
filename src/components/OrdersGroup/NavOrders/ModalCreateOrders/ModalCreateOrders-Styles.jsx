85f2d318ddab17ea2b5d229629454665b8c987b9
Brendo <brendow_prime@hotmail.com>
2024-04-18 02:13:37 -0400

    Feat: ModalCreateOrders on Orders component

--- src/components/OrdersGroup/NavOrders/ModalCreateOrders/ModalCreateOrders-Styles.jsx
+++ src/components/OrdersGroup/NavOrders/ModalCreateOrders/ModalCreateOrders-Styles.jsx
@@ -1,12 +1,5 @@
 import { styled } from "styled-components";
 
-export const Modal = styled.div`
-            background-color: white;
-            border-radius: 8px;
-            box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.5);
-            z-index: 9999;
-            `;
-
 export const Overlay = styled.div`
             position: fixed;
             top: 0;
@@ -19,40 +12,12 @@ export const Overlay = styled.div`
             justify-content: center;
             align-items: center;
             `;
-
-export const ModalForms = styled.div`
-    width: 1000px;
-    height: 250px;
-    margin-top: 20px;
-    padding: 0px 25px 25px 25px;
-    display: flex;
-    flex-wrap: wrap;
-    gap: 23px;
-    div{
-        display: flex;
-        flex-direction: column;
-        justify-content: center;
-        width: 300px;
-        height: 60px;
-        gap:8px;
-    }
-    h1{
-        color:#2F3036;
-        font-family: Inter;
-        font-size: 12px;
-        font-weight: 700;
-        line-height: 14.52px;
-        text-align: left;
-    }
-    input{
-        width: 300px;
-        height: 48px;
-        padding: 12px 16px;
-        border-radius: 12px;
-        border: 1.5px solid #C5C6CC;
-    }
-`;
-
+export const Modal = styled.div`
+            background-color: white;
+            border-radius: 8px;
+            box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.5);
+            z-index: 9999;
+            `;
 export const ModalTitle = styled.div`
     display: flex;
     align-items: center;
@@ -74,16 +39,84 @@ export const ModalTitle = styled.div`
         height: 20px;
         cursor: pointer;
     }
-`;
-
-export const Footer = styled.div`
-    height: 60px;
-    border-top: 1px solid #D9D9D9;
+`
+export const SelectArea = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: start;
+    gap: 20px;
+    margin-left: 25px;
+    margin-top: 20px;
+    input{
+        width: 300px;
+        height: 40px;
+        padding: 19px 16px;
+        background-color: #FFFFFF;
+        border: 1px solid #777e87;
+        border-radius: 8px;
+    }
+    h1{
+        font-family: Inter;
+        font-size: 15px;
+        font-weight: 800;
+        line-height: 16.94px;
+        text-align: left;
+        margin-bottom:10px;
+    }
+`
+export const StyledInput = styled.div`
     display: flex;
     align-items: center;
     justify-content: end;
-    padding-right: 30px;
+    input{
+        width: 300px;
+        height: 40px;
+        padding: 19px 16px;
+        background-color: #FFFFFF;
+        border: 1px solid #858c96;
+        border-radius: 8px;
+        position: relative;
+    }
+    input::placeholder {
+        font-family: Roboto;
+        font-size: 13px;
+        font-weight: 400;
+        line-height: 19px;
+        letter-spacing: 0em;
+        text-align: left;
+    }
+    img{
+        position: absolute;
+        margin-right: 20px;
+        cursor: pointer;
+    }
 `
+
+export const ProductList = styled.div`
+    width: 1070px;
+    height: 250px;
+    margin-top: 20px;
+    padding: 0px 25px 25px 25px;
+    display: flex;
+    flex-wrap: wrap;
+    gap: 23px;
+    h1{
+        color:#2F3036;
+        font-family: Inter;
+        font-size: 12px;
+        font-weight: 700;
+        line-height: 14.52px;
+        text-align: left;
+    }
+    input{
+        width: 300px;
+        height: 48px;
+        padding: 12px 16px;
+        border-radius: 12px;
+        border: 1.5px solid #C5C6CC;
+    }
+`;
+
 export const SaveButton = styled.div`
     display: flex;
     align-items: center;
@@ -102,4 +135,13 @@ export const SaveButton = styled.div`
         text-align: center;
         color: white
     }
+`
+export const Footer = styled.div`
+    height: 60px;
+    border-top: 1px solid #D9D9D9;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    padding-right: 30px;
+    padding-left: 30px;
 `
--- src/components/OrdersGroup/NavOrders/ModalCreateOrders/ModalCreateOrders.jsx
+++ src/components/OrdersGroup/NavOrders/ModalCreateOrders/ModalCreateOrders.jsx
@@ -1,9 +1,14 @@
-import { Footer, Modal, ModalForms, ModalTitle, Overlay, SaveButton } from "./ModalCreateOrders-Styles";
+import { Footer, Modal, ModalTitle, Overlay, ProductList, SaveButton, SelectArea, StyledInput } from "./ModalCreateOrders-Styles";
 import CloseIcon from "../../../../assets/images/close.svg";
+import { styled } from "styled-components";
+import SearchIcon from "../../../../assets/images/search.svg";
+import Image from "../../../../assets/images/Image.svg";
+import Minus from "../../../../assets/images/Minus.svg";
+import Add from "../../../../assets/images/Add.svg";
 
 export default function ModalCreateOrders(props) {
 
-    const {activeCreateOrdersModal} = props;
+    const { activeCreateOrdersModal } = props;
 
     return (
         <>
@@ -13,45 +18,139 @@ export default function ModalCreateOrders(props) {
                         <h2>Cadrastro de pedido</h2>
                         <img onClick={activeCreateOrdersModal} src={CloseIcon} alt="CloseIcon" />
                     </ModalTitle>
-                    <ModalForms>
+                    <SelectArea>
                         <div>
-                            <h1>Nome</h1>
-                            <input type="text" />
+                            <input type="search" placeholder="Selecionar cliente" />
                         </div>
                         <div>
-                            <h1>Nome</h1>
-                            <input type="text" />
+                            <h1>Produtos</h1>
+                            <StyledInput>
+                                <input type="text" placeholder="Pesquisar produtos" />
+                                <img src={SearchIcon} alt="SearchIcon" />
+                            </StyledInput>
                         </div>
-                        <div>
-                            <h1>Nome</h1>
-                            <input type="text" />
-                        </div>
-                        <div>
-                            <h1>Nome</h1>
-                            <input type="text" />
-                        </div>
-                        <div>
-                            <h1>Nome</h1>
-                            <input type="text" />
-                        </div>
-                        <div>
-                            <h1>Nome</h1>
-                            <input type="text" />
-                        </div>
-                        <div>
-                            <h1>Nome</h1>
-                            <input type="text" />
-                        </div>
-                        <div>
-                            <h1>Nome</h1>
-                            <input type="text" />
-                        </div>
-                        <div>
-                            <h1>Nome</h1>
-                            <input type="text" />
-                        </div>
-                    </ModalForms>
+                    </SelectArea>
+                    <ProductList>
+                        <Product>
+                            <img src={Image} alt="Image" />
+                            <Details>
+                                <TitleProduct>
+                                    <h1>Produto</h1>
+                                    <h2>Cód. 1</h2>
+                                </TitleProduct>
+                                <Resume>
+                                    <Quantity>
+                                        <img src={Minus} alt="Sub" />
+                                        <h1>1</h1>
+                                        <img src={Add} alt="Add" />
+                                    </Quantity>
+                                    <Price>
+                                        R$ 9,99
+                                    </Price>
+                                </Resume>
+                            </Details>
+                        </Product>
+                        <Product>
+                            <img src={Image} alt="Image" />
+                            <Details>
+                                <TitleProduct>
+                                    <h1>Produto</h1>
+                                    <h2>Cód. 1</h2>
+                                </TitleProduct>
+                                <Resume>
+                                    <Quantity>
+                                        <img src={Minus} alt="Sub" />
+                                        <h1>1</h1>
+                                        <img src={Add} alt="Add" />
+                                    </Quantity>
+                                    <Price>
+                                        R$ 9,99
+                                    </Price>
+                                </Resume>
+                            </Details>
+                        </Product>
+                        <Product>
+                            <img src={Image} alt="Image" />
+                            <Details>
+                                <TitleProduct>
+                                    <h1>Produto</h1>
+                                    <h2>Cód. 1</h2>
+                                </TitleProduct>
+                                <Resume>
+                                    <Quantity>
+                                        <img src={Minus} alt="Sub" />
+                                        <h1>1</h1>
+                                        <img src={Add} alt="Add" />
+                                    </Quantity>
+                                    <Price>
+                                        R$ 9,99
+                                    </Price>
+                                </Resume>
+                            </Details>
+                        </Product>
+                        <Product>
+                            <img src={Image} alt="Image" />
+                            <Details>
+                                <TitleProduct>
+                                    <h1>Produto</h1>
+                                    <h2>Cód. 1</h2>
+                                </TitleProduct>
+                                <Resume>
+                                    <Quantity>
+                                        <img src={Minus} alt="Sub" />
+                                        <h1>1</h1>
+                                        <img src={Add} alt="Add" />
+                                    </Quantity>
+                                    <Price>
+                                        R$ 9,99
+                                    </Price>
+                                </Resume>
+                            </Details>
+                        </Product>
+                        <Product>
+                            <img src={Image} alt="Image" />
+                            <Details>
+                                <TitleProduct>
+                                    <h1>Produto</h1>
+                                    <h2>Cód. 1</h2>
+                                </TitleProduct>
+                                <Resume>
+                                    <Quantity>
+                                        <img src={Minus} alt="Sub" />
+                                        <h1>1</h1>
+                                        <img src={Add} alt="Add" />
+                                    </Quantity>
+                                    <Price>
+                                        R$ 9,99
+                                    </Price>
+                                </Resume>
+                            </Details>
+                        </Product>
+                        <Product>
+                            <img src={Image} alt="Image" />
+                            <Details>
+                                <TitleProduct>
+                                    <h1>Produto</h1>
+                                    <h2>Cód. 1</h2>
+                                </TitleProduct>
+                                <Resume>
+                                    <Quantity>
+                                        <img src={Minus} alt="Sub" />
+                                        <h1>1</h1>
+                                        <img src={Add} alt="Add" />
+                                    </Quantity>
+                                    <Price>
+                                        R$ 9,99
+                                    </Price>
+                                </Resume>
+                            </Details>
+                        </Product>
+                    </ProductList>
                     <Footer>
+                        <Total>
+                            <h1>Total:</h1>
+                            <h2>R$ 49,99</h2>
+                        </Total>
                         <SaveButton>
                             <p>Salvar</p>
                         </SaveButton>
@@ -61,3 +160,118 @@ export default function ModalCreateOrders(props) {
         </>
     )
 }
+
+
+const Product = styled.div`
+    display: flex;
+    align-items: center;
+    width: 300px;
+    height: 100px;
+    gap: 0px;
+    border-radius: 16px;
+    border: 1px solid #D4D6DD;
+    opacity: 0px;
+    img{
+        width: 88px;
+        height: 100px;
+        margin-right: 20px;
+    }
+`
+
+const Details = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    width: 180px;
+    div{
+        display: flex;
+    }
+`
+const Resume = styled.div`
+    display: flex;
+    justify-content: space-between;
+`
+
+const Quantity = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    margin-top:10px;
+    gap: 5px;
+    img{
+        margin-right: 6px;
+        width: 24px;
+        height: 24px;
+        cursor:pointer; 
+    }
+    h1{
+        font-family: Inter;
+        font-size: 14px;
+        font-weight: 400;
+        line-height: 20px;
+        text-align: center;
+        color:#1F2024;
+        margin-right: 6px;
+    }
+
+`
+const Price = styled.h1`
+    display:flex;
+    align-items: end;
+    justify-content: center;
+    font-family: Inter;
+    font-size: 16px;
+    font-weight: 800;
+    line-height: 16.94px;
+    text-align: left;
+    color:#1F2024;
+    padding-bottom:5px;
+    padding-right: 10px;
+`
+const TitleProduct = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    align-items: flex-start;
+    width: 205px;
+    height: 35px;
+    gap: 4px;
+    h1{
+    font-family: Inter;
+    font-size: 12px;
+    font-weight: 700;
+    line-height: 14.52px;
+    text-align: left;
+    color:#1F2024;
+    }
+    h2{
+    font-family: Inter;
+    font-size: 12px;
+    font-weight: 400;
+    line-height: 16px;
+    letter-spacing: 0.01em;
+    text-align: left;
+    color: #71727A;
+    }
+`
+const Total = styled.div`
+    display: flex;
+    gap: 10px;
+    h1{
+        font-family: Inter;
+        font-size: 14px;
+        font-weight: 700;
+        line-height: 20px;
+        text-align: left;
+        color:#1ABC00
+    }
+    h2{
+        font-family: Inter;
+        font-size: 16px;
+        font-weight: 900;
+        line-height: 19.36px;
+        letter-spacing: 0.005em;
+        text-align: left;
+        color:#1F2024;
+    }
+`
